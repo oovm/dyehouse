130adf9d3412ff0632b3cb56fe523ab2445f2976
Aster <aster@vers.site>
2023-10-30 14:54:17 +0800

    Accelerate simple cases where the interval does not need to be modified

--- projects/code-span/src/view/iter.rs
+++ projects/code-span/src/view/iter.rs
@@ -1,5 +1,4 @@
-use std::collections::BTreeSet;
-use std::ops::Range;
+use std::{collections::BTreeSet, ops::Range};
 
 use crate::view::{CodeRender, InnerSpan};
 
@@ -21,34 +20,28 @@ pub struct CodeRendered<'r, 'i, 's> {
     iter: std::collections::btree_map::Iter<'r, usize, InnerSpan<'s>>,
 }
 
-pub struct CodeView<'i, 's> {
+#[derive(Copy, Clone, Debug)]
+pub struct CodeView<'r, 'i, 's> {
     pub text: &'i str,
-    pub kind: BTreeSet<&'s str>,
+    pub kind: &'r BTreeSet<&'s str>,
 }
 
-impl<'r, 'i, 's> IntoIterator for &'r CodeRender<'i, 's>
-{
-    type Item = CodeView<'i, 's>;
+impl<'r, 'i, 's> IntoIterator for &'r CodeRender<'i, 's> {
+    type Item = CodeView<'r, 'i, 's>;
     type IntoIter = CodeRendered<'r, 'i, 's>;
 
     fn into_iter(self) -> Self::IntoIter {
-        CodeRendered {
-            text: self.text,
-            iter: self.view.iter(),
-        }
+        CodeRendered { text: self.text, iter: self.interval.iter() }
     }
 }
 
-impl<'r, 'i, 's> Iterator for CodeRendered<'r, 'i, 's>
-{
-    type Item = CodeView<'i, 's>;
+impl<'r, 'i, 's> Iterator for CodeRendered<'r, 'i, 's> {
+    type Item = CodeView<'r, 'i, 's>;
 
     fn next(&mut self) -> Option<Self::Item> {
         let (start, span) = self.iter.next()?;
-        let range = Range { start: *start, end: *span.end };
-
-
-
+        let range = Range { start: *start, end: span.end };
+        let text = self.text.get(range.clone())?;
+        Some(CodeView { text, kind: &span.styles })
     }
 }
-
--- projects/code-span/src/view/mod.rs
+++ projects/code-span/src/view/mod.rs
@@ -1,14 +1,60 @@
-use std::collections::{BTreeMap, BTreeSet};
-use std::fmt::Debug;
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    fmt::Debug,
+    mem::take,
+    ops::Range,
+};
 
 mod iter;
 
+#[derive(Debug)]
 pub struct CodeRender<'i, 's> {
+    /// raw text
     text: &'i str,
-    view: BTreeMap<usize, InnerSpan<'s>>,
+    /// start -> (styles , end)
+    interval: BTreeMap<usize, InnerSpan<'s>>,
 }
 
+#[derive(Clone, Debug)]
 struct InnerSpan<'s> {
-    style: BTreeSet<&'s str>,
+    styles: BTreeSet<&'s str>,
     end: usize,
+}
+
+impl<'i, 's> CodeRender<'i, 's> {
+    pub fn new(text: &'i str) -> Self {
+        let mut interval = BTreeMap::new();
+        interval.insert(0, InnerSpan { styles: Default::default(), end: text.len() });
+        Self { text, interval }
+    }
+    pub fn mark_span(&mut self, span: Range<usize>, style: &'s str) {
+        match self.interval.get(&span.start) {
+            Some(_) => {
+                match self.interval.get(&span.end) {
+                    // easy case, no need modifier old interval
+                    Some(_) => self.interval.range_mut(span).for_each(|(_, inner)| {
+                        inner.styles.insert(style);
+                    }),
+                    None => {
+                        todo!()
+                    }
+                }
+            }
+            None => {
+                // 修改前一个区间的结束位置
+                self.interval.range_mut(..span.start).next_back().map(|(_, v)| {
+                    v.end = span.start;
+                });
+                todo!()
+            }
+        }
+    }
+}
+
+#[test]
+fn main() {
+    let mut code_render = CodeRender::new("public class Main {}");
+    code_render.mark_span(0..6, "keyword");
+    code_render.mark_span(7..12, "keyword");
+    println!("{:#?}", code_render);
 }
